53b4c0c9136003b44261985cf238c2c6dcafb48b
rakibtg <rakibtg@gmail.com>
2020-01-26 23:42:37 +0600

    basic layout with incomplete directory listing and monaco components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 
-// import MonacoEditor from "react-monaco-editor";
+import Monaco from "./components/Editor/Monaco";
+import DirectoryListing from "./components/FolderStructure/DirectoryListing";
 
 function App() {
   return (
-    <div className="App">
-      <p>haha</p>
+    <div className="full_height app_editor_wrapper">
+      <div className="sidebar_wrapper">
+        <DirectoryListing />
+      </div>
+      <div className="monaco_editors_wrapper">
+        <Monaco language="php" theme="vs-dark" />
+      </div>
     </div>
   );
 }
