adf03750daa8745b67ef94b0042f92b540182ae9
rakibtg <rakibtg@gmail.com>
2020-01-27 23:53:03 +0600

    a little work on backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,49 @@
-// Nothing here yet!
+const express = require("express");
+const cors = require("cors");
+const getPort = require("get-port");
+const deglob = require("deglob");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  const opts = {
+    cwd: "./",
+    useGitIgnore: false,
+    usePackageJson: false
+  };
+  deglob(["**/*.js"], opts, function(err, files) {
+    let nestedFiles = {};
+    files.map((file, index) => {
+      let partialSegment = "";
+      file.split(path.sep).map((segment, segmentIndex) => {
+        partialSegment = partialSegment + segment + path.sep;
+        if (!nestedFiles[partialSegment]) {
+          nestedFiles[partialSegment] = {
+            path: file,
+            label: segment,
+            partialSegment,
+            child: []
+          };
+        } else {
+          nestedFiles[partialSegment].child.push({
+            path: file,
+            label: segment,
+            partialSegment,
+            child: []
+          });
+        }
+      });
+    });
+    res.json({ "light-code": "Yes", nestedFiles });
+  });
+});
+
+getPort().then(port => {
+  app.listen(4000, () =>
+    console.log(`Example app listening on port http://localhost:4000!`)
+  );
+});
