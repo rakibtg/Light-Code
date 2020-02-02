57bf75048d672bf1cdaf7134d439edd83e9e35ca
rakibtg <rakibtg@gmail.com>
2020-02-02 21:25:35 +0600

    code cleanups

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const getPort = require("get-port");
-const deglob = require("deglob");
-const path = require("path");
-const { isFile } = require("path-type");
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+const fileExplorerController = require("./controllers/fileExplorer");
+
 app.get("/", async (req, res) => {});
+app.post("/files", fileExplorerController.index);
 
-const opts = {
-  cwd: "./",
-  useGitIgnore: true,
-  usePackageJson: false
-};
-deglob(["**/*.js"], opts, function(err, files) {
-  files.map((file, index) => {
-    console.log(path.parse(file));
-  });
+getPort().then(port => {
+  app.listen(4000, () =>
+    console.log(`Example app listening on port http://localhost:4000!`)
+  );
 });
-
-// getPort().then(port => {
-//   app.listen(4000, () =>
-//     console.log(`Example app listening on port http://localhost:4000!`)
-//   );
-// });
